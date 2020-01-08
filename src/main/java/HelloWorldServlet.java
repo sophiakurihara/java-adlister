e22722b39b5a60cb32f8f97d3781d0bcb9e7d75b
sophiakurihara <sophia.sudiacal@gmail.com>
2020-01-08 10:06:22 -0600

    Hello World Servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        String name = req.getParameter("name");
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        if(name != null){
+            out.println("<h1>Hello, " + name + "</h1>");
+        } else {
+            out.println("<h1>Hello, World!</h1>");
+        }
     }
 }
