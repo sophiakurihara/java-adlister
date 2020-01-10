e6a45f18f6aa66280e43303b6b6d9e8eb8b27cb3
sophiakurihara <sophia.sudiacal@gmail.com>
2020-01-10 09:09:38 -0600

    test

---
+++ src/main/java/LogoutServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res){
+
+    }
+}
