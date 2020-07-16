e2fe825caaa95002dd2556f1e22c50416adae8b5
sophiakurihara <sophia.sudiacal@gmail.com>
2020-07-15 21:15:12 -0500

    updated mvc solution to look like curriculum

--- src/main/java/ListAdsDao.java
+++ src/main/java/ListAdsDao.java
@@ -2,41 +2,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListAdsDao implements Ads {
-    private List<Ad> ads;
+    private List<Ad> ads = new ArrayList<>();
 
-    public List<Ad> all() {
-        if (ads == null) {
-            ads = generateAds();
-        }
-        return ads;
-    }
-
-    private List<Ad> generateAds() {
-        List<Ad> ads = new ArrayList<>();
-        ads.add(new Ad(
+    public ListAdsDao(){
+        insert(new Ad(
             1,
             1,
             "playstation for sale",
             "This is a slightly used playstation"
         ));
-        ads.add(new Ad(
+        insert(new Ad(
             2,
             1,
             "Super Nintendo",
             "Get your game on with this old-school classic!"
         ));
-        ads.add(new Ad(
+        insert(new Ad(
             3,
             2,
             "Junior Java Developer Position",
             "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
         ));
-        ads.add(new Ad(
+        insert(new Ad(
             4,
             2,
             "JavaScript Developer needed",
             "Must have strong Java skills"
         ));
+    }
+    public void insert(Ad ad){
+        this.ads.add(ad);
+    }
+
+    public List<Ad> all(){
         return ads;
     }
+//    public List<Ad> all() {
+//        if (ads == null) {
+//            ads = generateAds();
+//        }
+//        return ads;
+//    }
+//
+//    private List<Ad> generateAds() {
+//        List<Ad> ads = new ArrayList<>();
+//        ads.add(new Ad(
+//            1,
+//            1,
+//            "playstation for sale",
+//            "This is a slightly used playstation"
+//        ));
+//        ads.add(new Ad(
+//            2,
+//            1,
+//            "Super Nintendo",
+//            "Get your game on with this old-school classic!"
+//        ));
+//        ads.add(new Ad(
+//            3,
+//            2,
+//            "Junior Java Developer Position",
+//            "Minimum 7 years of experience required. You will be working in the scripting language for Java, JavaScript"
+//        ));
+//        ads.add(new Ad(
+//            4,
+//            2,
+//            "JavaScript Developer needed",
+//            "Must have strong Java skills"
+//        ));
+//        return ads;
+//    }
 }
